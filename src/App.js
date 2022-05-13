dbcf58c7fe36bcb4416a5226df32551050c02a0b
Aleksi Eskola <aleksi.eskola@atrsoft.com>
2022-05-13 15:30:57 +0300

    Tailwind commented

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import products from "./data/products.json";
 
 function App() {
   return (
-    <div className="bg-blue-light py-4 dark:bg-gray-800">
-      <div className="md:w-1/2 mx-2 md:mx-auto">
+    // bg-blue-light py-4 dark:bg-gray-800
+    <div className="">
+      {/* md:w-1/2 mx-2 md:mx-auto */}
+      <div className="">
         {products.map((product) => (
           <Product product={product} key={product.id} />
         ))}
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,25 @@
 function Product({ product: { title, price, description, category, image } }) {
   return (
-    <div className="flex bg-white w-full mb-5 shadow-sm rounded-lg dark:bg-gray-300 group hover:shadow-2xl transition-shadow duration-300">
-      <div className="w-5/12 p-2 overflow-hidden dark:bg-white rounded-tl-lg rounded-bl-lg">
+    // bg-white w-full mb-5 shadow-sm rounded-lg dark:bg-gray-300 group hover:shadow-2xl transition-shadow duration-300
+    <div className="flex ">
+      {/* p-2 overflow-hidden dark:bg-white rounded-tl-lg rounded-bl-lg*/}
+      <div className="w-5/12">
+        {/*bg-center transition-transform duration-300 group-hover:transform group-hover:scale-125*/}
         <div
           style={{ backgroundImage: `url(${image})` }}
-          className="bg-contain bg-no-repeat bg-center w-full h-full transition-transform duration-300 group-hover:transform group-hover:scale-125"
+          className="bg-contain bg-no-repeat w-full h-full "
         ></div>
       </div>
-      <div className="w-7/12 p-5">
-        <h1 className="md:text-2xl">{title.substr(0, 50)}</h1>
-        <h3 className="text-blue dark:text-gray-700">{category}</h3>
-        <p className="text-blue-shock mt-4 text-xl md:text-4xl">{price}$</p>
-        <div className="mt-4">{description.substr(0, 100)}</div>
+      {/*p-5*/}
+      <div className="w-7/12 ">
+        {/*md:text-2xl*/}
+        <h1 className="">{title.substr(0, 50)}</h1>
+        {/*text-blue dark:text-gray-700*/}
+        <h3 className="">{category}</h3>
+        {/*text-blue-shock mt-4 text-xl md:text-4xl*/}
+        <p className="">{price}$</p>
+        {/*mt-4*/}
+        <div className="">{description.substr(0, 100)}</div>
       </div>
     </div>
   );
